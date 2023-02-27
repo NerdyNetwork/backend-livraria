49dfa328d2674fd33062c35aaf9dad15823b9586
João Darwin <joaodarwin.ip22@gmail.com>
2023-02-27 12:48:50 -0300

    Criação da pasta para o BackEnd, e inicialização do backend em Java + SpringBoot

---
+++ backend/src/test/java/com/livraria/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livraria.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

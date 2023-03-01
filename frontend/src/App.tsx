3b0f2f8acca62f783096bd56ada1c4f397b2098e
JoaoPedroli <joaopedrocz12345@gmail.com>
2023-03-01 16:51:34 -0300

    update App.jsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,10 @@ import { Routes } from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import "./sass/global.scss";
 
-function App() {
+export const App = () => {
   return (
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
   );
 }
-
-export { App };

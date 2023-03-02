12777ae29a9a27ccad6ac86d7969a7a69ef37e4d
João Darwin <joaodarwin.ip22@gmail.com>
2023-03-02 11:32:45 -0300

    Desenvolvimento da Entity Pedido

--- backend/src/main/java/com/livraria/livraria/entities/Pedido.java
+++ backend/src/main/java/com/livraria/livraria/entities/Pedido.java
@@ -1,5 +1,96 @@
 package com.livraria.livraria.entities;
 
-public class Pedido {
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.livraria.livraria.entities.enums.StatusDoPedido;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido")
+@CrossOrigin("*")
+public class Pedido implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant moment;
+	
+	private Integer statusDoPedido;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
+	
+	public Pedido() {
+	}
+	
+	public Pedido(long id, Instant moment, StatusDoPedido statusDoPedido, Usuario usuario) {
+		this.id = id;
+		this.moment = moment;
+		setStatusDoPedido(statusDoPedido);
+		this.usuario = usuario;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public StatusDoPedido getStatusDoPedido() {
+		return StatusDoPedido.valueOf(statusDoPedido);
+	}
+
+	public void setStatusDoPedido(StatusDoPedido statusDoPedido) {
+		if(statusDoPedido != null) {
+			this.statusDoPedido = statusDoPedido.getCode();
+		}
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return id == other.id;
+	}
 }

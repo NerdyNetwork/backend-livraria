3f4682fc43a75608098cb0fc3a9af6be76476f4d
João Darwin <joaodarwin.ip22@gmail.com>
2023-03-02 12:42:36 -0300

    Desenvolvimento da Entity Autor

---
+++ backend/src/main/java/com/livraria/livraria/entities/Autor.java
@@ -0,0 +1,87 @@
+package com.livraria.livraria.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_autor")
+public class Autor implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String nome;
+	private String biografia;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "autor")
+	private Set<Livro> livros = new HashSet<>();
+	
+	public Autor() {
+	}
+	
+	public Autor(long id, String nome, String biografia) {
+		this.id = id;
+		this.nome = nome;
+		this.biografia = biografia;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getBiografia() {
+		return biografia;
+	}
+
+	public void setBiografia(String biografia) {
+		this.biografia = biografia;
+	}
+
+	public Set<Livro> getLivros() {
+		return livros;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return id == other.id;
+	}
+}

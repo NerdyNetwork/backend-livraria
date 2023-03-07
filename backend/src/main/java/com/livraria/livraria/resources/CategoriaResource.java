c4489e5117a10b7a8d3241c0fa7206b4fbba9cd7
João Darwin <joaodarwin.ip22@gmail.com>
2023-03-07 20:36:33 -0300

    [FEAT] - Desenvolvimento das camadas de Service e Resource da Entity Categoria

---
+++ backend/src/main/java/com/livraria/livraria/resources/CategoriaResource.java
@@ -0,0 +1,63 @@
+package com.livraria.livraria.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.livraria.livraria.entities.Categoria;
+import com.livraria.livraria.services.CategoriaService;
+import com.livraria.livraria.services.exceptions.ResourceNotFoundException;
+
+@RestController
+@RequestMapping(value = "/categoria")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
+		try {
+			Categoria categoria = categoriaService.findById(id);
+			return ResponseEntity.ok().body(categoria);
+		} catch(ResourceNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> findAll() {
+		List<Categoria> categorias = categoriaService.findAll();
+		return ResponseEntity.ok().body(categorias);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria) {
+		categoria = categoriaService.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+		categoriaService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Categoria> updareCategoria(@PathVariable Long id, @RequestBody Categoria novaCategoria) {
+		Categoria categoria = categoriaService.updateCategoria(id, novaCategoria);
+		return ResponseEntity.ok().body(categoria);
+	}
+}

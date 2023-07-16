30d9530252f89d04b1a5ae2dccab4905f1737777
João Darwin <joaodarwin.ip22@gmail.com>
2023-07-16 19:00:23 -0300

    refactor: melhorando retorno ResponseEntity e Tratamento de Exceções correto

--- backend/src/main/java/com/livraria/livraria/resources/AutorResource.java
+++ backend/src/main/java/com/livraria/livraria/resources/AutorResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.livraria.livraria.entities.Autor;
 import com.livraria.livraria.services.AutorService;
+import com.livraria.livraria.services.exceptions.DatabaseException;
 import com.livraria.livraria.services.exceptions.ResourceNotFoundException;
 
 @RestController
@@ -34,27 +36,41 @@ public class AutorResource {
 			Autor autor = autorService.findById(id);
 			return ResponseEntity.ok().body(autor);
 		} catch(ResourceNotFoundException e) {
-			return ResponseEntity.notFound().build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<Autor>> findAll() {
-		List<Autor> autores = autorService.findAll();
-		return ResponseEntity.ok().body(autores);
+		try {
+			List<Autor> autores = autorService.findAll();
+			return ResponseEntity.ok().body(autores);
+		} catch (DatabaseException err) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
 	}
 	
 	@PostMapping
 	public ResponseEntity<Autor> insert(@RequestBody Autor autor) {
-		autor = autorService.insert(autor);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autor.getId()).toUri();
-		return ResponseEntity.created(uri).body(autor);
+		try {
+			autor = autorService.insert(autor);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autor.getId()).toUri();
+			return ResponseEntity.created(uri).body(autor);
+		} catch (DatabaseException err) {
+			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+		}
 	}
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
-		autorService.deleteById(id);
-		return ResponseEntity.noContent().build();
+		try {
+			autorService.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} catch (ResourceNotFoundException err) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		} catch (DatabaseException err) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 	
 	@PutMapping(value ="/{id}")
@@ -63,7 +79,9 @@ public class AutorResource {
 			Autor autor = autorService.updateAutor(id, novoAutor);
 			return ResponseEntity.ok().body(autor);
 		} catch(ResourceNotFoundException e) {
-			return ResponseEntity.notFound().build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		} catch(DatabaseException err) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 	}
 }

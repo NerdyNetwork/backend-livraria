fa8e04fd9e31eb50853a785d739ee0ffc4f2a1fa
João Darwin <joaodarwin.ip22@gmail.com>
2023-07-21 22:40:59 -0300

    test: Criar um teste como base a serem implementados

--- backend/src/test/java/com/livraria/livraria/LivrariaApplicationTests.java
+++ backend/src/test/java/com/livraria/livraria/LivrariaApplicationTests.java
@@ -1,13 +1,29 @@
 package com.livraria.livraria;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.livraria.livraria.entities.Livro;
+import com.livraria.livraria.services.LivroService;
 
 @SpringBootTest
 class LivrariaApplicationTests {
 
+
+	@Autowired
+	private LivroService livroService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void findLivroById() {
+		long id = 2;
+		Livro livro = livroService.findById(id);
+		Assertions.assertEquals(2, livro.getId());
 	}
 
 }

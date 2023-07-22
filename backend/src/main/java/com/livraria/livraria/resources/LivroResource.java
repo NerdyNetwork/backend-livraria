e837745cd77e82ed6849aeaab250d026083e6bed
João Darwin <joaodarwin.ip22@gmail.com>
2023-07-21 22:42:09 -0300

    refactor: Remover importações não utilizadas no LivroService

--- backend/src/main/java/com/livraria/livraria/resources/LivroResource.java
+++ backend/src/main/java/com/livraria/livraria/resources/LivroResource.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

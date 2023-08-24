49f59df6ad187d725f20c28e091ad580947f6d40
João Darwin <joaodarwin.ip22@gmail.com>
2023-08-23 23:02:38 -0300

    refactor: Alterar DTO do livro para Record no lugar de Class

--- backend/src/main/java/com/livraria/livraria/entities/dtos/response/BookDTO.java
+++ backend/src/main/java/com/livraria/livraria/entities/dtos/response/BookDTO.java
@@ -2,40 +2,6 @@ package com.livraria.livraria.entities.dtos.response;
 
 import java.io.Serializable;
 
-public class BookDTO implements Serializable{
+public record BookDTO(Long id, String name, int amount) implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
-    private Long id;
-    private String name;
-    private int amount;
-
-    public BookDTO() {
-    }
-
-    public BookDTO(Long id, String name, int amount) {
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
-    }   
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
 }
